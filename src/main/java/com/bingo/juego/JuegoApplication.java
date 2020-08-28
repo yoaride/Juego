8484c8845a23969e462fec3a50596caa8b0786c4
Yorbel Ariza <yoaride@gmail.com>
2020-08-28 17:25:53 -0500

    inicio proyecto

---
+++ src/main/java/com/bingo/juego/JuegoApplication.java
@@ -0,0 +1,13 @@
+package com.bingo.juego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bingo/juego/JuegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingo.juego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
